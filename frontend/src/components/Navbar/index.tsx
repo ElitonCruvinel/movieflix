192a0fd63ce724860081c8710b09724cb43d7d7e
Eliton Cruvinel <eliton@netscape.net>
2023-02-20 21:53:46 -0300

    Navbar styling

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <nav className="nav-bar bg-primary main-nav">
+      <div>
+        <a href="link" className="nav-logo-text">
+          <h4>MovieFlix</h4>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
